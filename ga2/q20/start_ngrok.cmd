a247066a42df7e75177a1d7e3af2898ae9673d47
vaths <srivathsnat@gmail.com>
2026-02-28 18:52:28 +0530

    Reorganize repo: keep only ga3 and ga4, remove generated artifacts

--- ga2/q20/start_ngrok.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-ngrok http 11434 ^
-  --response-header-add "X-Email: 23f3001415@ds.study.iitm.ac.in" ^
-  --response-header-add "X-User-Email: 23f3001415@ds.study.iitm.ac.in" ^
-  --response-header-add "Access-Control-Expose-Headers: *" ^
-  --response-header-add "Access-Control-Allow-Origin: *" ^
-  --response-header-add "Access-Control-Allow-Headers: *"
--- ga2/q20/start_ollama.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set OLLAMA_ORIGINS=*
-set OLLAMA_HOST=0.0.0.0:11434
-"C:\Users\sriva\AppData\Local\Programs\Ollama\ollama.exe" serve
